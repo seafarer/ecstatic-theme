96af699c37ea43019838f5716cfaccbf16cb0423
Colin OBrien <colin@studiotaraz.com>
2014-04-30 11:33:31 -0600

    add autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,12 @@ module.exports = function(grunt) {
                 files: ['sass/**/*.{scss,sass}' ],
                 tasks: ['sass:dist']
             },
+            autoprefixer: {
+                // When this file changes
+                files: ['css/ecstatic.css'],
+                // Run this task
+                tasks: ['autoprefixer']
+            },
             livereload: {
                 files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css', 'images/**/*.{png,jpg,jpeg,gif,webp,svg}'],
                 options: {
@@ -22,9 +28,23 @@ module.exports = function(grunt) {
                     'css/ecstatic.css': 'sass/ecstatic.scss'
                 }
             }
+        },
+        autoprefixer: {
+
+            options: {
+                browsers: ['last 3 versions', '> 1%', 'ie 9']
+            },
+
+            dist: {
+                // prefix the specified file
+                files: {
+                    'css/ecstatic.css' : 'css/ecstatic.css'
+                }
+            }
         }
     });
-    grunt.registerTask('default', ['sass:dist', 'watch']);
+    grunt.registerTask('default', ['sass:dist', 'autoprefixer', 'watch']);
     grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-watch');
 };
