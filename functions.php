c92ee59fd0c13435a6207e24c15ce3ae5370fd0a
Colin OBrien <colin@studiotaraz.com>
2014-05-16 16:50:02 -0600

    filename change

--- functions.php
+++ functions.php
@@ -79,7 +79,7 @@ function ecstatic_scripts() {
   wp_enqueue_script( 'ecstatic-modernizr', get_template_directory_uri() . '/js/vendor/modernizr.custom.js', array(),'', false );
   wp_enqueue_script( 'ecstatic-navigation', get_template_directory_uri() . '/js/inc/navigation.js', array(), '20120206', true );
   wp_enqueue_script( 'ecstatic-skip-link-focus-fix', get_template_directory_uri() . '/js/inc/skip-link-focus-fix.js', array(), '20130115', true );
-  wp_enqueue_script( 'ecstatic-app', get_template_directory_uri() . '/js/ecstatic.js', array(), '1.0', true );
+  wp_enqueue_script( 'ecstatic-app', get_template_directory_uri() . '/js/app.js', array(), '1.0', true );
 
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
     wp_enqueue_script( 'comment-reply' );
